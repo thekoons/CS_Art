668b6570a8f8f9d870a8c65a10feb78740efcb82
TheDennenning <denning2@pdx.edu>
2020-02-18 12:54:36 -0800

    Implemented Cuts, Sorting over RGB

--- events.h
+++ events.h
@@ -11,6 +11,24 @@ using namespace std;
 #define _EVENT_HPP
 
 
+#define RED 1
+#define GREEN 2
+#define BLUE 3
+#define BRIGHT 4
+
+// Computes n choose s, efficiently
+double Binomial(int n, int s)
+{
+    double        res;
+
+    res = 1;
+    for (int i = 1 ; i <= s ; i++)
+        res = (n - i + 1) * res / i ;
+
+    return res;
+}// Binomial
+
+
 class Event {
 	public:
 		Event(Bitmap *b, int start, int duration) : _b(b), _start(start), _duration(duration) {}
@@ -27,27 +45,85 @@ class Event {
 class Sorter : public Event{
 
 	public:
-		Sorter(Bitmap *b, int start, int duration) : 
-			Event(b, start, duration) {}
+		Sorter(Bitmap *b, int start, int duration, int type, float* lower_bound, float* upper_bound) : 	Event(b, start, duration), _width(_b->getSize(1)), _height(_b->getSize(0)),
+			_lower_bound(lower_bound), _upper_bound(upper_bound), _type(type) {}
 
 		void Activate(int frame_num);
 
 	private:
 
+		int _type;
+
+		int _width;
+		int _height;
+
+		float* _lower_bound;
+		float* _upper_bound;
+
 		void sort_line(int line_num);
 
+		int getComp(vector<pair<int, Pixel*>> *arr, int index);
+
+		int partition(vector<pair<int, Pixel*>> *arr, int low, int high);
+		void quickSort(vector<pair<int, Pixel*>> *arr, int low, int high);
+		void blur();
+
+		int eval_lower(int n)
+			{ 
+				int val = (int) (n * _lower_bound[1] + _lower_bound[0]);
+
+				if (val < 0)				return 0;
+				if (val > _height - 1)		return _height - 1; 
+				else						return val;
+			}
+
+		int eval_upper(int n)
+			{ 
+				int val = (int) (n * _upper_bound[1] + _upper_bound[0]);
+
+				if (val < 0)				return 0;
+				if (val > _height - 1)		return _height - 1; 
+				else						return val;
+			}
 };
 
 void Sorter::Activate(int frame_num)
 {
 	sort_line(frame_num);
+
+#ifdef BLUR
+	if (frame_num + 1 >= _width)
+		blur();
+#endif
 }
 
-int partition(vector<pair<int, Pixel*>> *arr, int low, int high)
+int Sorter::getComp(vector<pair<int, Pixel*>> *arr, int index)
+{
+	int val = 0;
+
+	switch (_type) {
+		case RED:
+			val = (*arr)[index].second->getRGB(0);
+			break;
+		case GREEN:
+			val = (*arr)[index].second->getRGB(1);
+			break;
+		case BLUE:
+			val = (*arr)[index].second->getRGB(2);
+			break;
+		case BRIGHT:
+			val = (*arr)[index].second->getBRT();
+			break;
+	}
+
+	return val;
+}
+
+int Sorter::partition(vector<pair<int, Pixel*>> *arr, int low, int high)
 {
 
     // pivot (Element to be placed at right position)
-    int pivot = (*arr)[high].second->getBRT();  
+    int pivot = getComp(arr, high);  
  
     int i = (low - 1);  // Index of smaller element
 
@@ -55,7 +131,7 @@ int partition(vector<pair<int, Pixel*>> *arr, int low, int high)
     {
 
         // If current element is smaller than the pivot
-        if ((*arr)[j].second->getBRT() < pivot)
+        if (getComp(arr, j) < pivot)
         {
             i++;    // increment index of smaller element
 			(*arr)[i].swap((*arr)[j]);
@@ -67,7 +143,7 @@ int partition(vector<pair<int, Pixel*>> *arr, int low, int high)
     return (i + 1);
 }
 
-void quickSort(vector<pair<int, Pixel*>> *arr, int low, int high)
+void Sorter::quickSort(vector<pair<int, Pixel*>> *arr, int low, int high)
 {
 
     if (low < high)
@@ -94,9 +170,12 @@ void Sorter::sort_line(int line_num)
 
 #ifdef DEBUG
 	cout << "	Sort Executing..." << endl;
+
+	cout << "		bounds are: " << eval_lower(line_num) << ", " << eval_upper(line_num) << endl;
+
 #endif
 
-	quickSort(&pixels, 0, _b->getSize(0) - 1);
+	quickSort(&pixels, eval_lower(line_num), eval_upper(line_num));
 
 #ifdef DEBUG
 	cout << "	Extracting values..." << endl;
@@ -117,24 +196,108 @@ void Sorter::sort_line(int line_num)
 	cout << "	Plotting to Image..." << endl;
 #endif
 
-	for (int i = 0; i < _b->getSize(0); i++) 
-	{
-		Pixel* p = &_b->getPixel(i, line_num);
-#ifdef DEBUG
-		cout << "		pixel[i] = " << p->getRGB(0) << ", " << p->getRGB(1) << ", " << p-> getRGB(2)<< endl;
-
-		cout << "		temps[i] = " << temps[i][0] << ", " << temps[i][1] << ", " << temps[i][2] << endl;
-#endif
+	for (int i = 0; i < _b->getSize(0); i++)
 		_b->getPixel(i, line_num).setRGB(temps[i]);
-
-#ifdef DEBUG
-		cout << "		pixel[i] = " << p->getRGB(0) << ", " << p->getRGB(1) << ", " << p-> getRGB(2)<< endl;
-#endif
-	}
 
 #ifdef DEBUG
 	cout << "	Pixel Sort finished over line " << line_num << endl;
 #endif
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//		Helper Function for NPR_Paint
+//			Applies gaussian blur of size 2 * R + 1
+//
+///////////////////////////////////////////////////////////////////////////////
+void Sorter::blur()
+{
+	int N = 3;
+	double *Gaussian = new double[N];
+	double sum[3];
+	double total;
+
+	int*** temp = new int**[_height];
+
+	for (int i = 0; i < _height; i++)
+	{
+		temp[i] = new int*[_width];
+		for (int j = 0; j < _width; j++)
+		{
+			temp[i][j] = new int[3];
+		}
+	}
+
+	//Calculates 1D Gaussian
+	for (int k = 0; k < N; k++)
+		Gaussian[k] = Binomial(N, k);
+
+	cout << "		Initializing Gaussian Blur..." << endl;
+
+	//Loop through image
+	for (int i = 0; i < _height; i++)
+	{
+		for (int j = 0; j < _width; j++)
+		{
+			//Zero out summing variable
+			for (int k = 0; k < 3; k++)
+				sum[k] = 0;
+
+			total = 0;
+
+			int lb = -(N / 2);
+			int ub = (N / 2) + (N % 2);
+
+			//Computes value of filter over pixel (i, j) given sized filter N
+			for (int g_i = lb; g_i < ub; g_i++)
+			{
+				for (int g_j = lb; g_j < ub; g_j++)
+				{
+					//Skips values if out of bounds
+					if (i + g_i >= 0 && i + g_i < _height)
+					{
+						if (j + g_j >= 0 && j + g_j < _width)
+						{
+							//Gets pixel value at specified pixel
+							int* rgb = _b->getPixel(i + g_i, j + g_j).getRGB();
+
+							//Multiplies two 1-D gaussian filter terms together
+							//	Simulating 2-D gaussian filter
+							for (int k = 0; k < 3; k++)
+								sum[k] += Gaussian[g_i + (N / 2)] * Gaussian[g_j + (N / 2)] * rgb[k];
+
+							total += Gaussian[g_i + (N / 2)] * Gaussian[g_j + (N / 2)];
+						}
+					}
+				}
+			}
+
+			//Assigns value to temporary holding
+			//	So as to not mess with other filter calculations
+			for (int k = 0; k < 3; k++)
+				temp[i][j][k] = (int) (sum[k] / total); //weighted average
+		}
+	}
+
+	cout << "		Setting Image Values..." << endl;
+
+	//Empties and Deletes temporary holding
+	
+	for (int i = 0; i < _height; i++)
+	{
+		for (int j = 0; j < _width; j++)
+		{
+			for (int k = 0; k < 3; k++)
+				_b->getPixel(i, j).getRGB()[k] = temp[i][j][k];
+
+			delete[] temp[i][j];
+		}
+		delete[] temp[i];
+	}
+
+	delete[] temp;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -53,11 +53,139 @@ int main(int argc, char** argv)
 
         vector<Event*> events;
 
-        events.push_back(new Sorter(&image, 0, max_frame));
+        //FULL CUT
 
+        float* lower_bound_1 = new float[2];
+               lower_bound_1[0] = 0;
+               lower_bound_1[1] = 0;
+        float* upper_bound_1 = new float[2];
+               upper_bound_1[0] = height;
+               upper_bound_1[1] = 0;
+
+        events.push_back(new Sorter(&image, 0, max_frame, GREEN, lower_bound_1, upper_bound_1));
+
+/*
+        //CUT 7
+
+        float* lower_bound_1 = new float[2];
+               lower_bound_1[0] = height / 2;
+               lower_bound_1[1] = -0.5;
+        float* upper_bound_1 = new float[2];
+               upper_bound_1[0] = height / 2;
+               upper_bound_1[1] = -0.25;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_1, upper_bound_1));
+
+        float* lower_bound_2 = new float[2];
+               lower_bound_2[0] = height / 2;
+               lower_bound_2[1] = -0.25;
+        float* upper_bound_2 = new float[2];
+               upper_bound_2[0] = height / 2;
+               upper_bound_2[1] = 0.25;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_2, upper_bound_2));
+
+        float* lower_bound_3 = new float[2];
+               lower_bound_3[0] = height / 2;
+               lower_bound_3[1] = 0.25;
+        float* upper_bound_3 = new float[2];
+               upper_bound_3[0] = height / 2;
+               upper_bound_3[1] = 0.5;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_3, upper_bound_3));
+
+        //CUT 8
+
+        float* lower_bound_4 = new float[2];
+               lower_bound_4[0] = 0;
+               lower_bound_4[1] = 0;
+        float* upper_bound_4 = new float[2];
+               upper_bound_4[0] = 0;
+               upper_bound_4[1] = ((float) height) / width;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_4, upper_bound_4));
+*/
+/*
+        //Cut 6
+
+        float* lower_bound_1 = new float[2];
+               lower_bound_1[0] = height / 2;
+               lower_bound_1[1] = -0.5;
+        float* upper_bound_1 = new float[2];
+               upper_bound_1[0] = height;
+               upper_bound_1[1] = -0.5;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_1, upper_bound_1));
+*/
+/*
+        //CUTS 4/5
+
+        float* lower_bound_1 = new float[2];
+               lower_bound_1[0] = 0;
+               lower_bound_1[1] = 0;
+        float* upper_bound_1 = new float[2];
+               upper_bound_1[0] = height / 4;
+               upper_bound_1[1] = 0;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_1, upper_bound_1));
+
+        float* lower_bound_2 = new float[2];
+               lower_bound_2[0] = height / 3;
+               lower_bound_2[1] = 0;
+        float* upper_bound_2 = new float[2];
+               upper_bound_2[0] = 2 * height / 3;
+               upper_bound_2[1] = 0;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_2, upper_bound_2));
+
+        float* lower_bound_3 = new float[2];
+               lower_bound_3[0] = 2 * height / 3;
+               lower_bound_3[1] = 0;
+        float* upper_bound_3 = new float[2];
+               upper_bound_3[0] = height;
+               upper_bound_3[1] = 0;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_3, upper_bound_3));
+*/
+
+
+/*
+        //CUT 3
+
+        float* lower_bound_1 = new float[2];
+               lower_bound_1[0] = 0;
+               lower_bound_1[1] = 0;
+        float* upper_bound_1 = new float[2];
+               upper_bound_1[0] = -height / 2;
+               upper_bound_1[1] = 1;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_1, upper_bound_1));
+
+*/
+/*
+        //CUT 2
+
+        float* lower_bound_1 = new float[2];
+               lower_bound_1[0] = 0;
+               lower_bound_1[1] = 0;
+        float* upper_bound_1 = new float[2];
+               upper_bound_1[0] = 2 * height / 3;
+               upper_bound_1[1] = -0.5;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_1, upper_bound_1));
+
+        float* lower_bound_2 = new float[2];
+               lower_bound_2[0] = 6 * height / 5;
+               lower_bound_2[1] = -0.3;
+        float* upper_bound_2 = new float[2];
+               upper_bound_2[0] = height - 1;
+               upper_bound_2[1] = 0;
+
+        events.push_back(new Sorter(&image, 0, max_frame, lower_bound_2, upper_bound_2));
+*/
         for (int n = 0; n < max_frame; n++) {
 
-            events[0]->Activate(n);
+            for (auto e : events) e->Activate(n);
         
 #ifdef GIF
             getFrame(frame, image);
